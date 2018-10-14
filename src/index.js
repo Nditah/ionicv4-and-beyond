9e8932c1870b2a9132656cdea0d19bdc143fb31b
daviddalbusco <david.dalbusco@outlook.com>
2018-10-14 13:27:46 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/variables.css';
+import './css/theme.css';
+import './css/grid.css';
+import './css/popover.css';
+import './css/print.css';
+import './css/slider.css';
+
+import './scripts/fullscreen.js';
+import './scripts/print.js';
+import './scripts/slider.js';
+import './scripts/sliderJumpTo.js';
